4e88ec86ea5e7ca886f42cbfd3eff62eb52f07bb
rishugoyal805 <rishugoyal16800@gmail.com>
2025-04-13 23:49:11 +0530

    update page.jsx

--- app/signup/page.jsx
+++ app/signup/page.jsx
@@ -12,7 +12,7 @@ export default async function SignupPage() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-black">
       <div className="w-full max-w-md">
-      <img className="logo-login " src="/logo.jpg" />
+      {/* <img className="logo-login " src="/logo.jpg" /> */}
         <h1 className="mb-8 text-center text-3xl text-white font-bold">Create an Account</h1>
         <SignupForm />
         <p className="mt-4 text-center text-sm text-white">
