47467d240342095d20c8cc58f959b9b42f9a654b
rishugoyal805 <rishugoyal16800@gmail.com>
2025-04-13 23:31:53 +0530

    update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -6,15 +6,24 @@ import "./globals.css"
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata = {
-  title: "AI Chatbot",
+  title: "Askdemia",
   description: "A full-stack chatbot application with authentication",
-    generator: 'v0.dev'
+  icons: {
+    icon: "/logo.jpg",
+  },
 }
+
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={inter.className}>
+      <head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins&display=swap"
+          rel="stylesheet"
+        />
+      </head>
+      <body style={{ fontFamily: "'Poppins', sans-serif" }} className={inter.className} suppressHydrationWarning>
         <StyledEngineProvider injectFirst>
           <ThemeProvider>{children}</ThemeProvider>
         </StyledEngineProvider>
