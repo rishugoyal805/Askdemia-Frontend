5f5881c45f2634535b8dbcbe2a680014c421f444
rishugoyal805 <rishugoyal16800@gmail.com>
2025-04-13 23:33:31 +0530

    update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,7 @@
 import { redirect } from "next/navigation"
 import { getSession } from "@/lib/auth"
 import LoginForm from "@/components/auth/login-form"
-
+// import "/globals.css";
 export default async function Home() {
   const session = await getSession()
 
@@ -12,6 +12,7 @@ export default async function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-black">
       <div className="w-full max-w-md">
+        <img className="logo-login " src="/logo.jpg" />
         <h1 className="mb-8 text-center text-white text-3xl font-bold">Welcome to Askdemia</h1>
         <LoginForm />
         <p className="mt-4 text-center text-sm text-white">
